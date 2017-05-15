7359894573bf1bd8dbbde22d55e9cd5c6acfb73c
Michele Laurenti <laurenti.1603064@studenti.uniroma1.it>
2017-05-15 09:17:02 +0200

    Fix bug with invert_names function

--- twig/BibtexTwigExtension.php
+++ twig/BibtexTwigExtension.php
@@ -3,13 +3,6 @@ namespace Grav\Plugin\BibserverQueries;
 
 use Grav\Common\Grav;
 
-function invert_names($e) {
-    $names = explode(", ", $e);
-    $last_name = $names[0];
-    $first_name = $names[1];
-    return $first_name . " " . $last_name;
-}
-
 class BibtexTwigExtension extends \Twig_Extension
 {
     protected $grav;
@@ -39,10 +32,10 @@ class BibtexTwigExtension extends \Twig_Extension
         $authors_list = explode(" and ", $authors);
         $n_authors = count($authors_list);
         if ($n_authors == 1) {
-            return invert_names($authors);
+            return $this->invert_names($authors);
         } else {
             // swap first and last name, removing commas
-            $authors_list = array_map(invert_names, $authors_list);
+            $authors_list = array_map(array($this, 'invert_names'), $authors_list);
             // join authors with commas, 'and'
             $res = implode(", ", array_slice($authors_list, 0, $n_authors-1)) . " and " . $authors_list[$n_authors-1];
             return $res;
@@ -76,5 +69,12 @@ class BibtexTwigExtension extends \Twig_Extension
             $result[$year][] = $entry;
         }
         return $result;
+    }
+
+    protected function invert_names($e) {
+        $names = explode(", ", $e);
+        $last_name = $names[0];
+        $first_name = $names[1];
+        return $first_name . " " . $last_name;
     }
 }
